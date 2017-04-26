22450bd66e67558ebbe1b63a9b081df319d8cb36
Kevin Hill <khill@microsoft.com>
2017-04-25 19:05:52 -0700

    Added local data file for testing

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+var buildData = null;
+
+document.addEventListener("DOMContentLoaded", function(){
+    //Load Windows build data
+    getBuildData();
+});
+
+function getBuildData(){
+    // Create XHR object
+    var xhr = new XMLHttpRequest();                         
+    xhr.addEventListener("load", transferComplete);     
+    xhr.open("GET",                                     
+        "./data/data.json");
+    xhr.send(); 
+}
+
+function transferComplete(evt){
+    buildData = JSON.parse(evt.srcElement.responseText);
+    console.log(buildData);
+    document.getElementById("content").innerText = evt.srcElement.responseText;
+}
