d46858342c78d3746f8f6c9466d54a2d6655d5be
Kevin Hill <khill@microsoft.com>
2017-05-07 20:04:41 -0700

    getting activity data

--- scripts/activity.js
+++ scripts/activity.js
@@ -1,7 +1,29 @@
 // File: Activity.js
 // This file simulates a code activity feed
 'use strict'
+var activityData = null;
+
+// Sample function to get Windows Build data
+function getActivityData(file) {
+    // Create XHR object
+    var xhr = new XMLHttpRequest();
+    xhr.dataFile = file;
+    xhr.addEventListener("load", activityDataTransferComplete);
+    xhr.open("GET", file);
+    xhr.send();
+}
+
+function activityDataTransferComplete(evt){
+    activityData = JSON.parse(evt.srcElement.responseText);
+
+    showActivity();
+}
+
+function showActiviy(){
+    
+}
 
 document.addEventListener("DOMContentLoaded", function() {
-    
+    //Load Activity Data
+    getActivityData('./data/activity.json');
 });
