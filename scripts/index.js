63af13b589bba4f7d1d4c9ac31addaf3ab20ee9c
Jeff Burtoft <boyofgreen@gmail.com>
2017-05-08 22:07:53 -0700

    adjust loads

--- scripts/index.js
+++ scripts/index.js
@@ -10,13 +10,7 @@ var jsonFiles = new Array(); //["./data/17522_fbl_impressive.json", "./data/1752
 
 document.addEventListener("DOMContentLoaded", function(){
 
-  var ttme = 800;
-if (window.location.pathname.match('index')) ttme = 200;
-if (window.location.pathname.match('searchResults')) ttme = 200;
-if (window.location.pathname.match('fullDetails')) ttme = 200;
 
-  
-  setTimeout(function(){
 
   for(i = 0; i < fileNames.length; i++){
     jsonFiles.push(dataLocation + fileNames[i] + dataType);
@@ -58,7 +52,7 @@ if (window.location.pathname.match('fullDetails')) ttme = 200;
     searchView = false;
     detailedView = false;
   }
-  }, ttme)
+
 });
 
 // Identify which JSON files contain the data we are looking for!
